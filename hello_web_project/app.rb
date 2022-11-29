3ed745870b590ef9eecfcf38fcc6acd9746c7aa8
mahaque <haquema@outlook.com>
2022-11-29 12:44:59 +0000

    complete routing challenge again

--- hello_web_project/app.rb
+++ hello_web_project/app.rb
@@ -13,4 +13,11 @@ class Application < Sinatra::Base
 
     return "Hello #{name}"
   end
+
+  post '/submit' do
+    name = params[:name]
+    message = params[:message]
+
+    return "Thanks #{name}, you sent this message: \"#{message}\""
+  end
 end
